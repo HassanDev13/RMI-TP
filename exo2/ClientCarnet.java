ae5f697be0dd4904c1f657469648aa3e3ee6b2f9
hacene <zerrouk.mohammed.hacene@gmail.com>
2024-03-19 12:30:47 +0100

    fix client

--- exo2/ClientCarnet.java
+++ exo2/ClientCarnet.java
@@ -8,6 +8,15 @@ public class ClientCarnet {
 
             Carnet carnet1 = (Carnet) Naming.lookup("rmi://localhost:1099/Carnet1");
             Carnet carnet2 = (Carnet) Naming.lookup("rmi://localhost:1099/Carnet2");
+
+        
+            Adresse adresse = new Adresse("Birouana Number 10", "Tlemcen");
+            Adresse adresse2 = new Adresse("Amir Abdkadar Number 12", "Sabra");
+            Adresse adresse3 = new Adresse("Imama Number 55 A", "Tlemcen");
+
+            carnet1.enregistrer("Zerrouk Hacene", adresse);
+            carnet2.enregistrer("Ferradj Omar", adresse2);
+            carnet2.enregistrer("Ferradj Omar 2", adresse3);
             
             // list them all 
             System.out.println("Client 1 : " + carnet1.lister().size());
@@ -15,6 +24,7 @@ public class ClientCarnet {
 
             System.out.println("Search : " + carnet1.chercher("Zerrouk Hacene").getRue());
             System.out.println("Search : " + carnet2.chercher("Ferradj Omar").getRue());
+            
             // Use the carnet objects
         } catch (Exception e) {
             System.out.println("Client failed: " + e);
--- exo2/ServeurCarnet.java
+++ exo2/ServeurCarnet.java
@@ -4,15 +4,12 @@ import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
 public class ServeurCarnet {
+
+    
     public static void main(String[] args) {
         try {
             CarnetImpl carnet1 = new CarnetImpl();
             CarnetImpl carnet2 = new CarnetImpl();
-            Adresse adresse = new Adresse("Birouana Number 10", "Tlemcen");
-            Adresse adresse2 = new Adresse("Amir Abdkadar Number 12", "Sabra");
-
-            carnet1.enregistrer("Zerrouk Hacene", adresse);
-            carnet2.enregistrer("Ferradj Omar", adresse2);
 
             LocateRegistry.createRegistry(1099);
             Naming.rebind("rmi://localhost:1099/Carnet1", carnet1);
